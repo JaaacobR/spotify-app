c65e9feab86143133a0cbb2d86cc1777c3d49a8c
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-04-24 07:53:52 +0200

    Adding Text Component

--- src/screens/LogIn/LogIn.tsx
+++ src/screens/LogIn/LogIn.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import Text from '@core/components/Text';
 
 const LogIn: React.FC = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>AAA</Text>
+    </View>
+  );
 };
 
 export default LogIn;
