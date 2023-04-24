872d3d17e1472f5d6b0761a4feb828247fd0b05b
Jakub Ryszka <ryszkajakub6@gmail.com>
2023-04-24 08:23:59 +0200

    adding button component

--- src/screens/LogIn/LogIn.tsx
+++ src/screens/LogIn/LogIn.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 import Text from '@core/components/Text';
+import Button from '@core/components/Button';
 
 const LogIn: React.FC = () => {
   return (
     <View>
       <Text>AAA</Text>
+      <Button
+        onPress={() => {
+          console.log('Dupa');
+        }}>
+        example
+      </Button>
     </View>
   );
 };
